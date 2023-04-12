db501a298abe7f1ca35d8b26aa2c6d52213f4658
AirEsteban <airascaesteban@gmail.com>
2023-04-12 18:20:17 -0300

    Initial commit

---
+++ API_KEYCLOAK/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API_KEYCLOAK.Models;
+namespace API_KEYCLOAK.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        [Route("[controller]/getUsers")]
+        public IActionResult getAllUsers()
+        {
+            var users = new List<User>();
+            var user1 = new User { FirstName = "Juan", Id = 2 , LastName = "Airasca" };
+            var user2 = new User { FirstName = "Esteban", Id = 1, LastName = "Airasca"};
+            users.Add(user1);
+            users.Add(user2);
+
+            return Ok(users);
+        }
+    }
+}
